93e7b562540eb1d82f1735150a85a3a655442e91
rishabh <singh.rishabh30@gmail.com>
2020-07-08 00:37:15 +0530

    java script

---
+++ test.js
@@ -0,0 +1,54 @@
+// // var a=5;
+// // console.log(a);
+
+// // if(true)
+// // {
+// //     var a=10;
+// //     console.log(a);
+// // }
+
+// // console.log("===============");
+// // console.log(a);
+
+// // let a=5;
+// // if(true)
+// // {
+// //     let a=20
+// //     console.log(a);
+    
+// // }
+// // console.log("=====");
+// // console.log(a);
+
+
+
+// const pi=3.14;
+
+// if(true)
+// {
+//     const pi=2.14;
+//     console.log(pi);
+// }
+// console.log("+++++");
+// console.log(pi);\
+
+
+
+
+var x =function test(){
+    return "hi antu";
+};
+
+console.log(x);
+
+for()
+if
+else if
+
+advance Js  Promise, async await , callback ,  
+api 
+live api frontend
+postman
+mongodb
+reactjs
+nodejs express server 
